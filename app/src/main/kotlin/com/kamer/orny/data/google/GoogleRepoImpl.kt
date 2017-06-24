1c4b3aaea8a1b99e34846178bfdc63fae9d4c9bc
Dvoretskiy Maksim <mdvoretskiy@spaceotechnologies.ru>
2017-06-25 02:08:35 +0700

    Distinct mapper

--- app/src/main/kotlin/com/kamer/orny/data/google/GoogleRepoImpl.kt
+++ app/src/main/kotlin/com/kamer/orny/data/google/GoogleRepoImpl.kt
@@ -7,16 +7,13 @@ import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.model.ValueRange
 import com.kamer.orny.data.android.ReactiveActivities
-import com.kamer.orny.data.model.Author
+import com.kamer.orny.data.mapping.toExpense
+import com.kamer.orny.data.mapping.toList
 import com.kamer.orny.data.model.Expense
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Single
 import timber.log.Timber
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class GoogleRepoImpl(val googleAuthHolder: GoogleAuthHolder, val reactiveActivities: ReactiveActivities)
@@ -25,8 +22,6 @@ class GoogleRepoImpl(val googleAuthHolder: GoogleAuthHolder, val reactiveActivit
     companion object {
         private const val SPREADSHEET_ID = "1YsFrfpNzs_gjdtnqVNuAPPYl3NRjeo8GgEWAOD7BdOg"
         private const val SHEET_NAME = "Тест"
-
-        private val DATE_FORMAT = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
     }
 
     override fun getAllExpenses(): Single<List<Expense>> = getSheetsService()
@@ -72,7 +67,7 @@ class GoogleRepoImpl(val googleAuthHolder: GoogleAuthHolder, val reactiveActivit
         for (value in response.getValues()) {
             Timber.d(value.toString())
             if (value.isNotEmpty()) {
-                val expense = expenseFromList(value)
+                val expense = value.toExpense()
                 Timber.d(expense.toString())
                 list.add(expense)
             }
@@ -83,7 +78,7 @@ class GoogleRepoImpl(val googleAuthHolder: GoogleAuthHolder, val reactiveActivit
 
     private fun addExpenseToApi(service: Sheets, expense: Expense) {
         val writeData: MutableList<MutableList<Any>> = ArrayList()
-        writeData.add(expenseToList(expense))
+        writeData.add(expense.toList())
         val valueRange = ValueRange()
         valueRange.majorDimension = "ROWS"
         valueRange.setValues(writeData)
@@ -91,37 +86,6 @@ class GoogleRepoImpl(val googleAuthHolder: GoogleAuthHolder, val reactiveActivit
         Timber.d(request.toString())
         val response = request.execute()
         Timber.d(response.toString())
-    }
-
-    private fun expenseToList(expense: Expense): MutableList<Any> {
-        val dataRow: MutableList<Any> = ArrayList()
-        dataRow.add(expense.comment)
-        dataRow.add(DATE_FORMAT.format(expense.date))
-        dataRow.add(if (expense.isOffBudget) "1" else "0")
-        if (expense.author?.id != "0") {
-            dataRow.add("")
-        }
-        dataRow.add(expense.amount.toString())
-        return dataRow
-    }
-
-    private fun expenseFromList(list: MutableList<Any>): Expense {
-        val comment = list[0].toString()
-        val date = try {
-            DATE_FORMAT.parse(list[1].toString())
-        } catch (e: ParseException) {
-            Date()
-        }
-        val isOffBudget = list[2].toString() == "1"
-        val authorId = if (list[3].toString().isNullOrEmpty()) "1" else "0"
-        val amount = if (list[3].toString().isNullOrEmpty()) list[4].toString().toDouble() else list[3].toString().toDouble()
-        return Expense(
-                comment = comment,
-                date = date,
-                isOffBudget = isOffBudget,
-                author = Author(id = authorId, name = if (authorId == "0") "Лена" else "Макс", color = ""),
-                amount = amount
-        )
     }
 
 }
---
+++ app/src/main/kotlin/com/kamer/orny/data/mapping/ExpenseMapper.kt
@@ -0,0 +1,45 @@
+package com.kamer.orny.data.mapping
+
+import com.kamer.orny.data.model.Author
+import com.kamer.orny.data.model.Expense
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class ExpenseMapper {
+    companion object {
+        val DATE_FORMAT = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    }
+}
+
+fun Expense.toList(): MutableList<Any> {
+    val dataRow: MutableList<Any> = ArrayList()
+    dataRow.add(comment)
+    dataRow.add(ExpenseMapper.DATE_FORMAT.format(date))
+    dataRow.add(if (isOffBudget) "1" else "0")
+    if (author?.id != "0") {
+        dataRow.add("")
+    }
+    dataRow.add(amount.toString())
+    return dataRow
+}
+
+fun MutableList<Any>.toExpense(): Expense {
+    val comment = this[0].toString()
+    val date = try {
+        ExpenseMapper.DATE_FORMAT.parse(this[1].toString())
+    } catch (e: ParseException) {
+        Date()
+    }
+    val isOffBudget = this[2].toString() == "1"
+    val authorId = if (this[3].toString().isNullOrEmpty()) "1" else "0"
+    val amount = if (this[3].toString().isNullOrEmpty()) this[4].toString().toDouble() else this[3].toString().toDouble()
+    return Expense(
+            comment = comment,
+            date = date,
+            isOffBudget = isOffBudget,
+            author = Author(id = authorId, name = if (authorId == "0") "Лена" else "Макс", color = ""),
+            amount = amount
+    )
+}

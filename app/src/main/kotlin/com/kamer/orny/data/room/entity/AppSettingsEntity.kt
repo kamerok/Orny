6cc392ce042716960399c5be4f60f6196dc802e5
Dvoretskiy Maksim <mdvoretskiy@spaceotechnologies.ru>
2017-07-25 00:47:25 +0700

    Add app settings foreign key

--- app/src/main/kotlin/com/kamer/orny/data/room/entity/AppSettingsEntity.kt
+++ app/src/main/kotlin/com/kamer/orny/data/room/entity/AppSettingsEntity.kt
@@ -2,11 +2,22 @@ package com.kamer.orny.data.room.entity
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
 
-@Entity(tableName = "app_settings")
+@Entity(
+        tableName = "app_settings",
+        foreignKeys = arrayOf(
+                ForeignKey(
+                        entity = AuthorEntity::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("default_author_id"),
+                        onDelete = ForeignKey.SET_NULL
+                )
+        )
+)
 data class AppSettingsEntity(
         @PrimaryKey val id: Int = 0,
-        @ColumnInfo(name = "default_author_id") val defaultAuthorId: String
+        @ColumnInfo(name = "default_author_id") val defaultAuthorId: String?
 )
